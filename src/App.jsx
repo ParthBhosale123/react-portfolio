9f8c044672ee63a41d4fa118300070768edaa8fa
ParthBhosale123 <parthb00009@gmail.com>
2025-03-21 23:04:32 +0530

    Fixed case-sensitive import issue

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import About from "./components/About";
 import Technologies from "./components/Technologies";
 import Education from "./components/Education";
 import Projects from "./components/Projects";
-import Contact from "./components/contact";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
